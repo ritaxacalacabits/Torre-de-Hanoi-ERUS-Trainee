60569318cd53059113f89953b26a956b662998ba
ritaxacalacabits <drita5163@gmail.com>
2026-05-02 21:28:54 -0300

    Garra virou objeto

--- sketch_apr30a/sketch_apr30a.ino
+++ sketch_apr30a/sketch_apr30a.ino
@@ -1,99 +1,97 @@
 #include <Servo.h>
 
-// int vai guardar o ângulo. todos valores aqui sao ficticios
-int garraFecha = 0;
-int alturaSobe = 180;
-int baseEsq = 120;
-int baseDir = 60;
-int baseCen = 90;
-int garraAbre = 90;
-int alturaDesce = 0;
-
-
 //servo que vai representar o motor físico
 
-Servo servoGarra, servoOmbro, servoBase, servoCotovelo;
+class BracoRobo {
+    private: 
+    Servo servoGarra, servoOmbro, servoBase, servoCotovelo;
+
+    int garraFecha = 0;
+    int alturaSobe = 180;
+    int baseEsq = 120;
+    int baseDir = 60;
+    int baseCen = 90;
+    int garraAbre = 90;
+    int alturaDesce = 0;
+
+    void moverDisco(int origem, int destino){
+        girarPara(origem);
+        descendoGarra();
+        fecharGarra();
+        subindoGarra();
+        girarPara(destino);
+        descendoGarra();
+        abrirGarra();
+        subindoGarra();
+    }
+
+    
+
+    public:
+    
+    void iniciar(int pinoGarra, int pinoCotovelo, int pinoOmbro, int pinoBase){
+        servoGarra.attach(pinoGarra); // pino 9 do arduino (so coisa arbitraria ate aqui)
+        servoCotovelo.attach (pinoCotovelo);
+        servoOmbro.attach (pinoOmbro);
+        servoBase.attach (pinoBase);
+        pontoZero();
+         hanoi (3, baseEsq, baseDir, baseCen);
+
+    }
+    void pontoZero (){
+    servoCotovelo.write(alturaDesce);
+    servoGarra.write(garraFecha);
+    servoBase.write(baseCen);
+    servoOmbro.write(alturaDesce);
+
+    }
+
+    void abrirGarra (){
+    servoGarra.write(garraAbre); // manda o angulo la pro motor
+    }
+
+    void fecharGarra (){
+    servoGarra.write(garraFecha);
+    }
+
+    void subindoGarra (){
+    servoOmbro.write(alturaSobe);
+    servoCotovelo.write(alturaSobe);
+
+    }
+
+    void descendoGarra (){
+    servoOmbro.write(alturaDesce);
+    servoCotovelo.write(alturaDesce);
+    }
+
+
+    void girarPara (int angulo){
+        servoBase.write(angulo);
+    }
+
+    void hanoi (int n, int origem, int destino, int auxiliar){
+        if (n == 1){
+            moverDisco(origem,destino);
+
+        } else {
+            hanoi (n-1, origem, auxiliar, destino);
+            moverDisco(origem,destino);
+            hanoi(n-1, auxiliar, destino, origem);
+
+        }
+    }
+
+};
+
+BracoRobo braco; // instancia global
 
 //write e attach sao funçoes. write manda algo e attach sinaliza local.
 
-void pontoZero (){
-servoCotovelo.write(alturaDesce);
-servoGarra.write(garraFecha);
-servoBase.write(baseCen);
-servoOmbro.write(alturaDesce);
-
-}
-
-void abrirGarra (){
-servoGarra.write(garraAbre); // manda o angulo la pro motor
-}
-
-void fecharGarra (){
-servoGarra.write(garraFecha);
-}
-
-void subindoGarra (){
-servoOmbro.write(alturaSobe);
-servoCotovelo.write(alturaSobe);
-
-}
-
-void descendoGarra (){
-servoOmbro.write(alturaDesce);
-servoCotovelo.write(alturaDesce);
-}
-
-
-void girarPara (int angulo){
-    servoBase.write(angulo);
-}
-
-void hanoi (int n, int origem, int destino, int auxiliar){
-    if (n == 1){
-        girarPara(origem);
-        descendoGarra();
-        fecharGarra();
-        subindoGarra();
-        girarPara(destino);
-        descendoGarra();
-        abrirGarra();
-        subindoGarra();
-
-    } else {
-        hanoi (n-1, origem, auxiliar, destino);
-        girarPara(origem);
-        descendoGarra();
-        fecharGarra();
-        subindoGarra();
-        girarPara(destino);
-        descendoGarra();
-        abrirGarra();
-        subindoGarra();
-
-        hanoi(n-1, auxiliar, destino, origem);
-
-    }
-}
-
-
 void setup() {
-
-  
-        servoGarra.attach(9); // pino 9 do arduino (so coisa arbitraria ate aqui)
-        servoCotovelo.attach (3);
-        servoOmbro.attach (5);
-        servoBase.attach (6);
-    
-        pontoZero();
-        
-
-        hanoi (3, baseEsq, baseDir, baseCen);
-  
+braco.iniciar(9,3,5,6); 
     }
  
-
-
-
 void loop() {
  
 
